93fb8b8744bdab835f12474af1e963e89525c6fc
Gorthdzx <krislaksana123@gmail.com>
2024-04-06 09:00:11 +0700

    feat : Implement clas of Animal

---
+++ src/main/java/org/example/interfaces/Bird.java
@@ -0,0 +1,33 @@
+package org.example.interfaces;
+
+/*
+Bird class implement Trackable and extend Animal
+ */
+public class Bird extends Animal implements Trackable, FlightEnable {
+
+    @Override
+    public void move() {
+        System.out.println(getClass().getSimpleName() + "Hey wingss");
+    }
+
+    @Override
+    public void track() {
+        System.out.println(getClass().getSimpleName() + "Bird have a track");
+    }
+
+
+    @Override
+    public void takeOff() {
+        System.out.println(getClass().getSimpleName() + "The bird is take off");
+    }
+
+    @Override
+    public void land() {
+        System.out.println(getClass().getSimpleName() + "The bird is Landing");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("The bird is fly");
+    }
+}
