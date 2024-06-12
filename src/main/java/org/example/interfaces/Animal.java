c431670bb42fc4da4a1ac8894eccda38963a960e
Gorthdzx <krislaksana123@gmail.com>
2024-06-12 18:03:43 +0700

    feat: enum and interfaces

--- src/main/java/org/example/interfaces/Animal.java
+++ src/main/java/org/example/interfaces/Animal.java
@@ -1,6 +1,78 @@
 package org.example.interfaces;
 
-class Satelite implements OrbitEarth{
+//implements constants list
+enum FlightStages implements Trackable {
+    GROUNDED, LAUNCH, CRUISE, DATA_COLLECTION;
+
+    @Override
+    public void track() {
+        if (this != GROUNDED) {
+            System.out.println("Monitoring " + this);
+        }
+    }
+
+    public FlightStages getNextStages() {
+        FlightStages[] flightStages = values();
+        return flightStages[(ordinal() + 1) % flightStages.length];
+    }
+}
+
+interface OrbitEarth extends FlightEnable {
+    void achieveOrbit();
+}
+
+/*
+extending an interface
+ */
+
+interface FlightEnable {
+
+    double MILES_TO_KM = 1.59595;
+    double KM_TO_MILES = 1.44324;
+
+    void move();
+
+    void takeOff();
+
+    void land();
+
+    void fly();
+
+    default void transitions(FlightStages stages) {
+        System.out.println("Transition not implemented on " + this.getClass().getName());
+    }
+
+}
+
+interface Trackable {
+    void track();
+
+}
+
+record Dragon(String name, String type) implements FlightEnable {
+
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void takeOff() {
+
+    }
+
+    @Override
+    public void land() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
+
+class Satelite implements OrbitEarth {
 
     @Override
     public void achieveOrbit() {
@@ -26,28 +98,6 @@ class Satelite implements OrbitEarth{
     public void fly() {
 
     }
-}
-
-/*
-extending an interface
- */
-
-
-interface OrbitEarth extends FlightEnable{
-    void achieveOrbit();
-}
-
-
-interface FlightEnable {
-    void move();
-
-    void takeOff();
-    abstract void land();
-    void fly();
-}
-
-interface Trackable {
-     void track();
 }
 
 public abstract class Animal {
