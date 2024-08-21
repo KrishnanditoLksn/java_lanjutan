b1ce728d9bf4e389af8af428cc2fcd8bae765c4f
Gorthdzx <krislaksana123@gmail.com>
2024-08-21 20:47:07 +0700

    feat: leetcode 724

--- src/main/java/Playground.java
+++ src/main/java/Playground.java
@@ -216,4 +216,24 @@ public class Playground {
         }
         return res;
     }
+
+    public static int pivotIndex(int[] nums) {
+        int totalSum = 0;
+        int leftsum = 0;
+
+        //fill a total sum of all elements
+        for (int num : nums) {
+            totalSum += num;
+        }
+        //loop through elements
+        for (int i = 0; i < nums.length; i++) {
+            //check if all sum equals to a left sum if not
+            if (totalSum - leftsum - nums[i] == leftsum) {
+                return i;
+            }
+            //fill a left sum with nums element
+            leftsum += nums[i];
+        }
+        return -1;
+    }
 }
