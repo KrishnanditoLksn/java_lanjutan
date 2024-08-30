6f455ce784659086a888703e5edd2cca630e313e
Gorthdzx <krislaksana123@gmail.com>
2024-08-30 11:27:26 +0700

    feat: leetcode 3190

--- src/main/java/Playground.java
+++ src/main/java/Playground.java
@@ -255,4 +255,14 @@ public class Playground {
         }
         return characterStack.isEmpty();
     }
+
+    public static int minimumOperations(int[] nums) {
+        int res = 0;
+        for (int num : nums) {
+            if (num % 3 != 0) {
+                res++;
+            }
+        }
+        return res;
+    }
 }
