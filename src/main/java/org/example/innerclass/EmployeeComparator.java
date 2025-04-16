9a4da576fc8f16d51f51049e2985a669820b0a57
KrishnanditoLksn <krislaksana123@gmail.com>
2025-04-16 09:55:52 +0700

    feat: inner class with local variables  ✨

--- src/main/java/org/example/innerclass/EmployeeComparator.java
+++ src/main/java/org/example/innerclass/EmployeeComparator.java
@@ -1,5 +1,7 @@
 package org.example.innerclass;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,6 +29,52 @@ class Main {
         storeEmployees.sort(comparator);
         for (StoreEmployee e : storeEmployees) {
             System.out.println(e);
+        }
+
+        System.out.println("With Pig Latin name ");
+        addPigLatinName(storeEmployees);
+    }
+
+    public static void addPigLatinName(List<? extends StoreEmployee> list) {
+
+        String lastName = "Piggy";
+
+
+        class DecoratedEmployee extends StoreEmployee implements Comparable<DecoratedEmployee> {
+            private String pigLatinName;
+            private Employee originalInstance;
+
+
+            public DecoratedEmployee(String pigLatinName, Employee originalInstance) {
+                this.pigLatinName = pigLatinName + lastName;
+                this.originalInstance = originalInstance;
+            }
+
+            @Override
+            public String toString() {
+                return originalInstance.toString() + pigLatinName;
+            }
+
+            @Override
+            public int compareTo(@NotNull DecoratedEmployee o) {
+                return pigLatinName.compareTo(o.pigLatinName);
+            }
+        }
+
+        List<DecoratedEmployee> newList = new ArrayList<>(list.size());
+
+        for (var employee : list) {
+            String name = employee.getName();
+            String pigLatin = name.substring(1) + name.charAt(0) + "ay";
+            newList.add(new DecoratedEmployee(pigLatin, employee));
+        }
+
+        /*
+         If  null was passed  , it will use comparable compare to method
+         */
+        newList.sort(null);
+        for (var employee2 : newList) {
+            System.out.println(employee2.originalInstance.getName() + " " + employee2.pigLatinName);
         }
     }
 }
