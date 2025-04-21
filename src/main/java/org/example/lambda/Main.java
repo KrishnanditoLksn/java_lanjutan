2f7f341fdf9559ead9f1cce9199b5cf4a4a98a7b
KrishnanditoLksn <krislaksana123@gmail.com>
2025-04-21 20:55:40 +0700

    feat: biConsumer and predicate lambda ✨

--- src/main/java/org/example/lambda/Main.java
+++ src/main/java/org/example/lambda/Main.java
@@ -1,9 +1,7 @@
 package org.example.lambda;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.function.BiConsumer;
 
 public class Main {
     public static void main(String[] args) {
@@ -56,7 +54,7 @@ public class Main {
         people.forEach((s) -> System.out.println(s));
 
 
-        String[] name = {"Adri", "Damar", "Step"};
+        String[] name = {"Adri", "Damar", "Step", "Danar", "Viko"};
 
 
         List<String> peoples = new ArrayList<>(Arrays.asList(name));
@@ -74,8 +72,7 @@ public class Main {
          *   this lambda override a method
          */
 
-        int res = calculator((var a, var b) -> {
-            return a + b;}, 5, 2);
+        int res = calculator(Integer::sum, 5, 2);
         var res2 = calculator((var value1, var value2) -> value1 / value2, 5.20, 3.10);
         var res3 = calculator(
                 (a, b) -> a.toUpperCase() + " " + b.toUpperCase(), "Joko", "Elted"
@@ -84,6 +81,38 @@ public class Main {
         System.out.println(res3);
         System.out.println(res);
         System.out.println(res2);
+
+        var coords = Arrays.asList(
+                new double[]{23.123123, 31.3123},
+                new double[]{20.3221312, 71.75675},
+                new double[]{63.75675645, 91.6775}
+        );
+        /*
+         *Consumer does not return anything and take 1 parameter
+         */
+        coords.forEach((s) -> System.out.println(Arrays.toString(s)));
+
+        BiConsumer<Double, Double> pi = (lat, lng) -> System.out.printf("[lat:%3f  lon:%3f]%n", lat, lng);
+        var firstPoint = coords.getFirst();
+        System.out.println("--");
+        processPoint(firstPoint[0], firstPoint[1],
+                (lat, lng) -> System.out.printf("[lat:%3f  lon:%3f]%n", lat, lng));
+
+
+        System.out.println("--");
+        coords.forEach((s) -> processPoint(s[0], s[1], pi));
+
+        //Predicate functional interface and return a  boolean result
+        peoples.removeIf(
+                s -> s.equalsIgnoreCase("adri")
+        );
+
+        peoples.removeIf(s -> s.startsWith("V"));
+
+        peoples.forEach(System.out::println);
+
+
+        peoples.replaceAll(s -> s.charAt(0) + " - "  + s.toUpperCase(Locale.ROOT));
     }
 
     public static <T> T calculator(Operation<T> function, T value1, T value2) {
@@ -91,4 +120,13 @@ public class Main {
         System.out.println("Result of Operation" + result);
         return result;
     }
+
+    /**
+     * BiConsumer does not return anything and takes 2  parameter
+     */
+    public static <T> void processPoint(T t1, T t2, BiConsumer<T, T> consumer) {
+        consumer.accept(t1, t2);
+    }
+
+
 }
